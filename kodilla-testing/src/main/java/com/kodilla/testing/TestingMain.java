b1cbf969a61ba68fdf7f0aa83624d5cfd9bdddfc
Krzysztof Grzegorowski <k-grzegorowski@wp.pl>
2022-10-12 19:43:36 +0200

    6.2 Added first test method

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
+import java.util.Objects;
+
 public class TestingMain {
 
-    public static void main (String[] args) {
 
-        System.out.println("Moduł 6: Wprowadzenie do testowania oprogramowania");
+    public static void main(String[] args) {
+
+
+
+        Calculator calculator = new Calculator();
+
+        int resultAdd = calculator.add(4, 4); {
+            System.out.println(resultAdd);
+        }
+
+               int resultSubtract = calculator.subtract(8,4); {
+            System.out.println(resultSubtract);
+        }
+
+        if (Objects.equals(resultSubtract, 4)) {
+            System.out.println("test ok!");
+        }else {
+            System.out.println("Error!");
+        }
+
+        if (Objects.equals(resultAdd, 8)) {
+            System.out.println("Test Ok!");
+        } else {
+            System.out.println("Error");
+        }
     }
 }
