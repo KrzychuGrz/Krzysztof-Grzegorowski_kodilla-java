07e48ff99e066cc652db0ee4b3aade239cdd0aae
Krzysztof Grzegorowski <k-grzegorowski@wp.pl>
2022-10-27 13:25:23 +0200

    7.2 Add Stream and ForumUser final class

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,6 +3,7 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main (String [] args) {
@@ -14,5 +15,7 @@ public class StreamMain {
         poemBeautifier.beautify(() -> System.out.println("*** " + poem + " ***"));
         poemBeautifier.beautify(() -> System.out.println("A teraz przed państwem próbka poezji krasnoludzkiej: " + poem));
 
+        System.out.println("Using stream to generate even numbers form 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
